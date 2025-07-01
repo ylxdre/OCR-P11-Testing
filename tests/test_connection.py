40226a45a64b0b79c71124644728c0ef8d56ca3e
yann <yann@needsome.coffee>
2025-07-01 09:52:24 +0200

    removed print, unused decode

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -10,7 +10,6 @@ def test_should_status_code_ok(client):
 def test_should_display_sorry_with_unknown_email(client):
     email = "test@test.com"
     response = client.post('/showSummary', data={"email": email})
-    data = response.data.decode()
     assert "_flashes" in session
     assert session["_flashes"] == [("message", "Sorry, that email wasn't found")]
 
@@ -19,5 +18,5 @@ def test_shoul_display_page_on_known_email(client):
     email = "admin@irontemple.com"
     response = client.post('/showSummary', data={"email": email})
     soup = BeautifulSoup(response.data, 'html.parser')
-    assert soup.h2.text == "Welcome, admin@irontemple.com"
+    assert soup.h2.text == "Welcome, "+email
 
