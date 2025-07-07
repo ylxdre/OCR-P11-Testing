c26fee3fbd12e5538809aeb72de54e327119f93c
yann <yann@needsome.coffee>
2025-07-07 15:49:13 +0200

    made test more simple

--- tests/test_purchase.py
+++ tests/test_purchase.py
@@ -4,38 +4,43 @@ from flask import session
 
 class TestPoints:
     
-    def test_should_nok_when_too_much_points(self, client, connect, club1):
+    def test_should_nok_when_too_much_points(self, client, connect, club2):
         points = int(connect.span.text)
-        club1.update({"places": points+1})
-        response = client.post('/purchasePlaces', data=club1)
+        club2.update({"places": points+1})
+        response = client.post('/purchasePlaces', data=club2)
         soup = BeautifulSoup(response.data, "html.parser")
         assert "You don't have enough points" == soup.li.text
 
-    def test_should_ok_when_enough_points(self, client, connect, club1):
+    def test_should_ok_when_enough_points(self, client, connect, club2):
         points = int(connect.span.text)
-        club1.update({"places": points-1})
-        response = client.post('/purchasePlaces', data=club1)
+        # One remaining point after that :
+        club2.update({"places": points-1})
+        response = client.post('/purchasePlaces', data=club2)
         soup = BeautifulSoup(response.data, "html.parser")
-        assert f"Great ! "+str(points-1)+" places booked for "+club1['competition'] == soup.li.text
+        assert f"Great ! "+str(points-1)+" places booked for "+club2['competition'] == soup.li.text
+
+    def test_book_more_than_one_should_be_refused(self, client, connect, club2):
+        points = int(connect.span.text)
+        assert int(points) == 1 
 
 
 class TestPlaces:
 
-    def test_should_refuse_more_12_once(self, client, club1):
-        club1.update({"places": 13})
-        response = client.post('/purchasePlaces', data=club1)
+    def test_should_refuse_more_12_once(self, client, club2):
+        club2.update({"places": 13})
+        response = client.post('/purchasePlaces', data=club2)
         soup = BeautifulSoup(response.data, "html.parser")
         assert "You can't book more than 12 places" == soup.li.text
 
-    def test_should_refuse_more_12_total(self, client, club1):
-        club1.update({"places": 2})
-        response = client.post('/purchasePlaces', data=club1)
+    def test_should_refuse_more_12_total(self, client, club2):
+        club2.update({"places": 1})
+        response = client.post('/purchasePlaces', data=club2)
         soup = BeautifulSoup(response.data, "html.parser")
-        assert "Great ! 2 places booked for "+club1['competition'] == soup.li.text 
-        club1.update({"places": 12})
-        response = client.post('/purchasePlaces', data=club1)
+        assert "Great ! 1 places booked for "+club2['competition'] == soup.li.text 
+        club2.update({"places": 12})
+        response = client.post('/purchasePlaces', data=club2)
         soup = BeautifulSoup(response.data, "html.parser")
-        assert "You already booked 12 places for "+club1['competition'] == soup.li.text
+        assert "You already booked 12 places for "+club2['competition'] == soup.li.text
 
         
 class TestDate:
